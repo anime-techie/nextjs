a8058c530f5dfb4705302aea8dbcf6e0b4bc7543
Rakesh Yadav <rakesh.u.yadav786@gmail.com>
2021-09-29 17:00:23 +0530

    cors-error5

--- config/index.js
+++ config/index.js
@@ -2,5 +2,6 @@ export const API_URL =
 	process.env.NEXT_PUBLIC_API_URL ||
 	'https://nextjs-front-to-back.herokuapp.com';
 export const NEXT_URL =
-	process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+	process.env.NEXT_PUBLIC_FRONTEND_URL ||
+	'https://nextjstest-sooty-one.vercel.app';
 export const PER_PAGE = 5;
