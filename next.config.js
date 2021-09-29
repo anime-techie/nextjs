f961f3f4aed35b2bc8b6c07111cd0e0b5ef17756
Rakesh Yadav <rakesh.u.yadav786@gmail.com>
2021-09-29 16:38:11 +0530

    cors error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
 	images: {
 		domains: ['res.cloudinary.com'],
+		async rewrites() {
+			return [
+				{
+					source: '/api/*',
+					destination: 'https://*',
+				},
+			];
+		},
 	},
 };
